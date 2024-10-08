97c1de7a2671e6eedaee6cf595c48f29a1fd71e6
Nelson D. Quintana <114194685+SeathWR@users.noreply.github.com>
2024-10-08 12:31:25 -0500

    Add files via upload

---
+++ afeitadoras.sql
@@ -0,0 +1,13 @@
+SELECT TOP (1000) [orden]
+      ,[Producto]
+      ,[Precio]
+      ,[error]
+      ,[reseñas]
+      ,[Puntaje]
+      ,[calificacion]
+  FROM [Afeitadoras ].[dbo].[AFEITADORAS]
+
+
+
+
+  ALTER TABLE AFEITADORAS DROP COLUMN error;
